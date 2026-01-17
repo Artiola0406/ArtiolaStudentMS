8574fb12457f56338644fef3f88acce2ce1bd764
artiola0406 <artiola.qollaku@umib.net>
2026-01-17 20:26:12 +0100

    u krijua klasa professor

--- StudentMS/Professor.cs
+++ StudentMS/Professor.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentMS
+{
+    public class Professor : Person
+    {
+        public string Department { get; set; }
+        public string OfficeNumber { get; set; }
+        public List<string> CoursesTeaching { get; set; } // Lëndët që jep
+
+        public Professor(string name, string department, string officeNumber, string id)
+            : base(name, Person.GenerateEmail(name), id)
+        {
+            Department = department;
+            OfficeNumber = officeNumber;
+            CoursesTeaching = new List<string>();
+        }
+
+        public void AddCourse(string courseCode)
+        {
+            if (!CoursesTeaching.Contains(courseCode))
+            {
+                CoursesTeaching.Add(courseCode);
+            }
+        }
+
+        public bool RemoveCourse(string courseCode)
+        {
+            return CoursesTeaching.Remove(courseCode);
+        }
+
+        public override string ToString()
+        {
+            return $"Prof. {Name} - {Department} (Office: {OfficeNumber}) - Courses: {string.Join(", ", CoursesTeaching)}";
+        }
+    }
+}
